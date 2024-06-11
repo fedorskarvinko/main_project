8a59110337f60f8702b1c9fd3b10e96bdf87885f
Fedor Skarvinko <fedorskarvinko@gmail.com>
2024-06-11 23:33:56 +0400

    add func mask_account_card

--- src/widget.py
+++ src/widget.py
@@ -1,13 +1,26 @@
+from src.masks import get_mask_card_number
+
+
 def mask_account_card(account_card: str) -> str:
     """
     Функция принимает на вход строку с информацией — тип карты/счета и номер карты/счета,
     возвращает исходную строку с замаскированным номером карты/счета.
     """
-    parts = account_card.split()
-    if parts[0] in ["Visa", "Maestro"]:
-        return f"{parts[0]} {parts[1]} {parts[2]} {parts[3][:2]}** **** {parts[5]}"
-    elif parts[0] == "Счет":
+    parts = account_card.split(" ")
+    if parts[0] == "Счет":
         return f"{parts[0]} **{parts[1][-4:]}"
+    else:
+        mask_card = get_mask_card_number(int(parts[-1]))
+        del parts[-1]
+        name_card = " ".join(parts)
+        return f"{name_card} {mask_card} "
 
 
-#def get_data(data: str) -> str:
+def get_data(data: str) -> str:
+    """
+    Функция преобразует дату из ГГГ-ММ-ДД в ДД-ММ-ГГГГ
+    """
+    return f"{data[8:10]}.{data[5:7]}.{data[0:4]}"
+
+
+print(mask_account_card("Visa Platinum 8990922113665229"))
