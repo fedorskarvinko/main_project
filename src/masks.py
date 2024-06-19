610936cc5e16b796ab1f86e54e0925c3a2f4d22b
Fedor Skarvinko <fedorskarvinko@gmail.com>
2024-06-20 01:44:57 +0400

    fix masks.py

--- src/masks.py
+++ src/masks.py
@@ -3,7 +3,7 @@ def get_mask_card_number(card_number: int) -> str:
     Функция принимает на вход номер карты и возвращает ее маску.
     """
     str_card_number = str(card_number)
-    return f"{str_card_number[0:4]} {str_card_number[4:6]} **** ** {str_card_number[-4:]}"
+    return f"{str_card_number[0:4]} {str_card_number[4:6]}** **** {str_card_number[-4:]}"
 
 
 def get_mask_account(account: int) -> str:
