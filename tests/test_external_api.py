12b09ae7bc5c54493cdb8a80b95249ad99e71ebf
Fedor Skarvinko <fedorskarvinko@gmail.com>
2024-07-06 03:22:17 +0400

    fix external_api and test_external_api

--- tests/test_external_api.py
+++ tests/test_external_api.py
@@ -12,18 +12,18 @@ def convert_transaction_amount(transaction):
     """Функция, которая принимает на вход транзакцию и возвращает сумму транзакции в рублях"""
     amount = transaction["amount"]
     currency = transaction["currency"]
-    url = "https://api.apilayer.com/exchangerates_data/latest?symbols=usd%2C%20eur&base=rub"
+    url = "https://api.apilayer.com/exchangerates_data/convert"
     payload = {}
     headers = API_KEY
     if currency == "RUB":
-        return amount
+        return float(amount)
     elif currency != "RUB":
         response = requests.request("GET", url, headers={"apikey": API_KEY}, data=payload)
         print(response.json())
         if response.status_code == 200:
             rate = response.json()["rates"][currency]
             amount = round(amount / rate, 2)
-            return amount
+            return float(amount)
 
 
 @patch("requests.request")
@@ -36,7 +36,7 @@ def test_convert_transaction_amount(mock_requests):
     assert convert_transaction_amount(transaction) == 86.96
     mock_requests.assert_called_once_with(
         "GET",
-        "https://api.apilayer.com/exchangerates_data/latest?symbols=usd%2C%20eur&base=rub",
+        "https://api.apilayer.com/exchangerates_data/convert",
         headers={"apikey": API_KEY},
         data={},
     )
